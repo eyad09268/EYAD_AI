32587f6a908112200196540c88fa7c2afce6102a
eyad09268 <121633380+eyad09268@users.noreply.github.com>
2025-04-03 13:05:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyCO1nnUS3W8SU4OZKwq-N34knv6lAPTwu8"; // Your API key here
+const API_KEY = "AIzaSyD9n3CP_b10aYjGQtWtHtzONXAzDzFs1Ws"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
